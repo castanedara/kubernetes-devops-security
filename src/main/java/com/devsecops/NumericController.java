5c890d60499dd75b849beb977ce57ba6ffb7513e
castanedara <60078813+castanedara@users.noreply.github.com>
2024-07-05 07:09:44 -0500

    Update NumericController.java
    message

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -27,7 +27,7 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "Kubernetes DevSecOps";
+			return "Kubernetes DevSecOps Ronald";
 		}
 
 		@GetMapping("/compare/{value}")
